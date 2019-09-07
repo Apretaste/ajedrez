6adbe03eb696189d31118529e245f7de5ca51f28
kumahacker <kumahavana@gmail.com>
2019-09-07 15:41:42 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 function showToast(text) {
   M.toast({
     html: text
@@ -9,27 +8,27 @@ var fritz = {
   step: 0,
   start: null,
   end: null,
-  checkStep: function(){
+  checkStep: function () {
     return solutionData[this.step].start === this.start
         && solutionData[this.step].end === this.end;
   }
 };
 
-$(function(){
-  $(".fritz-cell").click(function(){
+$(function () {
+  $(".fritz-cell").click(function () {
     if (fritz.step < solutionData.length) {
       var id = $(this).attr('id');
-      if (fritz.start === null){
+      if (fritz.start === null) {
         fritz.start = id;
         $(this).addClass('blink_me');
-      } else {
+      }
+      else {
         fritz.end = id;
-        if (fritz.checkStep())
-        {
-          showToast('Bien!!!');
+        if (fritz.checkStep()) {
 
-          var fs = $("#" +  fritz.start);
-          var fe = $("#" +  fritz.end);
+
+          var fs = $("#" + fritz.start);
+          var fe = $("#" + fritz.end);
 
           fritz.step++;
 
@@ -42,11 +41,14 @@ $(function(){
           fritz.end = null;
 
           if (fritz.step > solutionData.length) {
-            showToast('Felicitaciones!');
+            showToast('Fin de la partida. Felicitaciones!');
           }
-
-        } else {
-          $("#" +  fritz.start).removeClass('blink_me');
+          else {
+            showToast('Bien!!!');
+          }
+        }
+        else {
+          $("#" + fritz.start).removeClass('blink_me');
           showToast('Mal !!');
           fritz.start = null;
         }
